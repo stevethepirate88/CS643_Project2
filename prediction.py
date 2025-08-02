2723c21519489d806a81d3339324fa2ec9e0ca2b
ScraneGames <100911537+ScraneGames@users.noreply.github.com>
2025-08-01 20:55:55 -0400

    starting to make things roll

--- prediction.py
+++ prediction.py
@@ -1,1 +1,14 @@
 # This is where I am going to write the prediction code
+import sys
+import argparse
+
+# Need to take the pathname to the test file as a command line parameter. This pathname contains the directory location and the filename of the test file.
+
+parser = argparse.ArgumentParser(description='Getting the filepath for training')
+parser.add_argument("path", help="The path to the files that will be used for training and validation. Don't include the file name.")
+path = parser.parse_args()
+
+
+# May use the ValidationDataset.csv as the test
+
+# The output needs to be a measure of prediction performance, specifically the F1 score available in MLlib
--- training.py
+++ training.py
@@ -2,8 +2,12 @@
 
 import argparse
 
-# The program needs to accept the path to the test file as a command line parameter.
+# The program needs to accept TrainingDataset.csv and ValidationDataset.csv
 parser = argparse.ArgumentParser(description='Getting the filepath for training')
 parser.add_argument("path", help="The path to the files that will be used for training and validation. Don't include the file name.")
 path = parser.parse_args()
+
+# Need to run training in parallel on multiple EC2 instances
+
+# Need to validate the model seperately
 
