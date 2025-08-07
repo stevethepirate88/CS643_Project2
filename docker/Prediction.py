b71b0552c0e86971e073cfc26ef87530d3e8dafb
ScraneGames <100911537+ScraneGames@users.noreply.github.com>
2025-08-07 00:55:27 -0400

    Added Docker Jawns

---
+++ docker/Prediction.py
@@ -0,0 +1,72 @@
+
+import sys
+
+from pyspark import SparkConf, SparkContext, SQLContext
+from pyspark.mllib.evaluation import MulticlassMetrics
+from pyspark.mllib.regression import LabeledPoint
+
+from pyspark.ml import PipelineModel
+from pyspark.mllib.linalg import Vectors
+from pyspark.ml.tuning import CrossValidatorModel
+from pyspark.ml.classification import LogisticRegressionModel, RandomForestClassificationModel
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
+
+
+# Declare the Spark context
+conf = (SparkConf().setAppName("Predict wine app"))
+sc = SparkContext("local", conf=conf)
+sc.setLogLevel("ERROR")
+sqlContext = SQLContext(sc)
+
+# Check to make sure there are arguments. We want to know what dataset we're running against the model
+if len(sys.argv) == 2:
+    testFile = sys.argv[1]
+
+testFile = sys.argv[0]
+
+def assemble_features(data_frame, label="quality"):
+    input_features = [col for col in data_frame.columns if col != label]
+    assembler = VectorAssembler(inputCols=input_features, outputCol="features")
+    return assembler.transform(data_frame).select("features", label)
+
+
+print("=-=-=-=-=- I'm not having -A- glass of wine, Stan, I'm having 6. It's called a tasting and it's classy. -=-=-=-=-=")
+
+# Declare and load the model from S3
+model = CrossValidatorModel.load("./WineModel")
+
+
+# Create the DataFrame
+input_df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true', sep=';').load("./ValidationDataset.csv")
+
+updated_validate = [
+    col_name.strip().replace('"""', '').replace('""', '').replace('"', '')
+    for col_name in input_df.columns
+]
+
+input_df = input_df.toDF(*updated_validate)
+input_df = input_df.withColumnRenamed('quality', 'label')
+
+outputRdd = input_df.rdd.map(lambda row: LabeledPoint(row[-1], Vectors.dense(row[:11])))
+
+
+
+predictions = model.transform(input_df)
+predictions_and_labels = predictions.select("prediction", "label")
+labelsAndPredictions = predictions_and_labels.rdd.map(lambda row:(row.label, row.prediction))
+
+evaluator = MulticlassClassificationEvaluator(
+    labelCol="label",
+    predictionCol="prediction",
+    metricName="weightedFMeasure"  # This specifically calculates weighted F1
+)
+weighted_f1 = evaluator.evaluate(predictions)
+
+metrics = MulticlassMetrics(labelsAndPredictions)
+
+# It's all the statistics!
+
+
+print("\n\n=-=-=-=-=- Well after all that wine, here are your results! -=-=-=-=-=")
+print(f"Weighted F1 Score = {weighted_f1:.4f}")
---
+++ docker/Training.py
@@ -0,0 +1,112 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler, Normalizer, StandardScaler
+from pyspark.ml.classification import LogisticRegression, RandomForestClassifier
+
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.ml import Pipeline
+
+# Declare the Spark session
+spark = SparkSession \
+    .builder \
+    .appName("CS643_Project") \
+    .getOrCreate()
+
+## Load Training Dataset
+
+trains3 = "s3a://cs643proj2/TrainingDataset.csv"
+validates3 = "s3a://cs643proj2/ValidationDataset.csv"
+train_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load(trains3)
+validation_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load(validates3)
+
+print("Data loaded from S3 bucket.")
+print(train_df.toPandas().head())
+
+# def remove_quotations(s):
+#     return s.replace('"', '')
+
+
+# Remove those pesky quotations
+
+updated_train = [
+    col_name.strip().replace('"""', '').replace('""', '').replace('"', '')
+    for col_name in train_df.columns
+]
+
+updated_validate = [
+    col_name.strip().replace('"""', '').replace('""', '').replace('"', '')
+    for col_name in validation_df.columns
+]
+
+train_df = train_df.toDF(*updated_train)
+train_df = train_df.withColumnRenamed('quality', 'label')
+
+validation_df = validation_df.toDF(*updated_validate)
+validation_df = validation_df.withColumnRenamed('quality', 'label')
+
+print("Data has been formatted.")
+print(train_df.toPandas().head())
+
+# Vectors.... ASSEMBLE!
+assembler = VectorAssembler(
+    inputCols=["fixed acidity",
+               "volatile acidity",
+               "citric acid",
+               "residual sugar",
+               "chlorides",
+               "free sulfur dioxide",
+               "total sulfur dioxide",
+               "density",
+               "pH",
+               "sulphates",
+               "alcohol"],
+                outputCol="inputFeatures")
+
+scaler = Normalizer(inputCol="inputFeatures", outputCol="features")
+
+# Testing two different ways of getting our model
+lr = LogisticRegression()
+rf = RandomForestClassifier()
+
+pipeline1 = Pipeline(stages=[assembler, scaler, lr])
+pipeline2 = Pipeline(stages=[assembler, scaler, rf])
+
+paramgrid = ParamGridBuilder().build()
+
+evaluator = MulticlassClassificationEvaluator(metricName="f1")
+
+crossval = CrossValidator(estimator=pipeline1,  
+                         estimatorParamMaps=paramgrid,
+                         evaluator=evaluator, 
+                         numFolds=3
+                        )
+
+cvModel1 = crossval.fit(train_df) 
+print("F1 Score for LogisticRegression Model: ", evaluator.evaluate(cvModel1.transform(validation_df)))
+
+
+crossval = CrossValidator(estimator=pipeline2,  
+                         estimatorParamMaps=paramgrid,
+                         evaluator=evaluator, 
+                         numFolds=3
+                        )
+
+cvModel2 = crossval.fit(train_df) 
+print("F1 Score for RandomForestClassifier Model: ", evaluator.evaluate(cvModel2.transform(validation_df)))
+
+winner = "None"
+
+# Let's see which one won!
+if evaluator.evaluate(cvModel1.transform(validation_df))  > evaluator.evaluate(cvModel2.transform(validation_df)):
+    
+    winner = "LogisticRegression"
+    
+    cvModel1.save("training/WineModel")
+    
+if evaluator.evaluate(cvModel2.transform(validation_df))  > evaluator.evaluate(cvModel1.transform(validation_df)):
+    
+    winner = "RandomForest"
+    cvModel2.save("s3a://cs643proj2/WineModel")
+
+
+print("The best prediction model we have is " + winner)
